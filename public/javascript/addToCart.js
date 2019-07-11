60d4c056ff7040d8b62f9376101a651865d238a8
mircea <mircea.constantin@outlook.com>
2019-07-11 16:32:01 +0300

    Final fix for Updating qty in cart

--- public/javascript/addToCart.js
+++ public/javascript/addToCart.js
@@ -1,17 +1,35 @@
+if (document.readyState !== 'loading') {
+  console.log('document is already ready, just execute code here');
+
+} else {
+  document.addEventListener('DOMContentLoaded', function () {
+    console.log('document was not ready, place code here');
+  });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-  let badgeCart = document.querySelector('#badgeCart');
-  localStorage.getItem('myCart')
-  var totalQty = 0;
-  var data = JSON.parse(localStorage.getItem('myCart'))
-  for (let i in data) {
-    totalQty += data[i].qty;
-  }
-  badgeCart.innerHTML = totalQty;
+  updateQtyCart()
 });
 
+
+function updateQtyCart() {
+  let badgeCart = document.querySelector('#badgeCart');
+  let totalQty = 0;
+  localStorage.getItem('myCart')
+  let data = JSON.parse(localStorage.getItem('myCart'))
+  for (let i in data) {
+    totalQty += Number(data[i].qty);
+  }
+  if (totalQty == 0) {
+    badgeCart.innerHTML = ''
+  } else {
+    badgeCart.innerHTML = totalQty;
+  }
+}
+
 function createPopUp(title, qty, poster) {
-  var notificationArea = document.querySelector('#notice');
-  var notificationTitle = document.createElement('div');
+  let notificationArea = document.querySelector('#notice');
+  let notificationTitle = document.createElement('div');
   notificationTitle.classList.add("noticePanel")
   notificationTitle.innerHTML = `
   <div class="media">
@@ -28,25 +46,25 @@ function createPopUp(title, qty, poster) {
 }
 
 function addToCart(id, title, poster, price) {
-  var qty = document.querySelector('#qty');
+  let qty = document.querySelector('#qty');
   if (qty == undefined) {
     qty = 1;
   } else {
-    qty = qty.value;
+    qty = Number(qty.value);
   }
-  var cart = {
+  let cart = {
     productID: id,
     productTitle: title,
     qty: qty,
     productPrice: price
   }
   if (localStorage.getItem('myCart')) {
-    var checkGame = false;
-    var data = JSON.parse(localStorage.getItem('myCart'))
+    let checkGame = false;
+    let data = JSON.parse(localStorage.getItem('myCart'))
     for (let i = 0; i < data.length; i++) {
       if (data[i].productID == id) {
         checkGame = true;
-        data[i].qty++
+        data[i].qty += qty;
       }
     }
     if (checkGame == false) {
@@ -54,14 +72,9 @@ function addToCart(id, title, poster, price) {
     }
     localStorage.setItem('myCart', JSON.stringify(data))
   } else {
+    console.log('else')
     localStorage.setItem('myCart', JSON.stringify([cart]))
   }
-
-  let badgeCart = document.querySelector('#badgeCart');
-  var totalQty = 0;
-  for (let i in data) {
-    totalQty += data[i].qty;
-  }
-  badgeCart.innerHTML = totalQty;
+  updateQtyCart();
   createPopUp(title, qty, poster)
 }
